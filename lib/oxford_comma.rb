c51eb648ad51d5f4e9bb6e9a68c898e5c0d4b87c
Ming Duan <11203duan@gmail.com>
2019-02-02 22:50:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+  array.join
+end
 
+def oxford_comma(arr)
+  if arr.length < 3
+    arr.join(' and ')
+  else
+    name = arr.pop
+    arr2 = arr.join(', ')
+    arr3 = arr2 << ", and "
+    arr3 << name
+    arr3
+  end
 end
